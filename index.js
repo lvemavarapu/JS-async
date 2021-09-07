e2b70456322f59545da5cd554a184cedb552e176
lvemavarapu <lvemavarapu@gmail.com>
2021-09-08 09:43:17 +1000

    async

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
-let alertButton = document.querySelector("#showAlert")
+let quoteP = document.querySelector(".quote")
+let sourceP = document.querySelector(".source")
+let quoteButton = document.getElementById("showQuote")
+let sourceButton = document.getElementById("showSource")
 
-alertButton.addEventListener('click', ()=>{
-    setTimeout(()=>alert("ALERT IS SHOWN AFTER THE TIMER !!"), 5000)
+quoteButton.addEventListener('click', ()=>{
+    if (quoteP.style.display == 'none'){
+        quoteButton.innerText = "Hide quote"
+        quoteP.style.display = 'block'
+    } else{
+        quoteButton.innerText = "Show quote"
+        quoteP.style.display = 'none'
+    }
+})
+
+const wait = (ms) =>{
+    let start = Date.now(); //initialising start current time
+    let now = start; //initialising now with start value
+    while (now - start < ms){
+        now = Date.now() //update now's value with the "new current time"
+    }
+}
+
+sourceButton.addEventListener('click', ()=>{
+    setTimeout(()=>{
+        if (sourceP.style.display == 'none'){
+            sourceButton.innerText = "Hide source"
+            sourceP.style.display = 'block'
+        } else{
+            sourceButton.innerText = "Show source"
+            sourceP.style.display = 'none'
+        }
+    }, 5000)
+    
 })
